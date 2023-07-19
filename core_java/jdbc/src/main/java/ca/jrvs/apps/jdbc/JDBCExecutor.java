ab4d84e533b369b5ac35487e050df30a91c49a83
drkcn94 <derekm.chanchan@gmail.com>
2023-07-19 18:40:45 +0000

    added implementations up to limit

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class JDBCExecutor {
     public static void main(String[] args) {
@@ -13,29 +14,7 @@ public class JDBCExecutor {
         try {
             Connection connection = dcm.getConnection();
 
-            OrderDAO orderDAO = new OrderDAO(connection);
-            Order order = orderDAO.findById(1000);
-            System.out.println(order);
-/*
-            CustomerDAO customerDAO = new CustomerDAO(connection);
-            Customer customer = new Customer();
-            customer.setFirstName("Eric");
-            customer.setLastName("Chang");
-            customer.setEmail("testmail@askjeeves.com");
-            customer.setAddress("148 Yonge St");
-            customer.setCity("Toronto");
-            customer.setState("ON");
-            customer.setPhone("905-289-4512");
-            customer.setZipCode("L5R8Y3");
 
-            Customer dbCustomer = customerDAO.create(customer);
-            System.out.println(dbCustomer);
-            dbCustomer = customerDAO.findById(dbCustomer.getId());
-            System.out.println(dbCustomer);
-            dbCustomer.setEmail("e.chang@mails.com");
-            dbCustomer = customerDAO.update(dbCustomer);
-            System.out.println(dbCustomer);
-            customerDAO.delete(dbCustomer.getId());*/
 
 
         } catch(SQLException e) {
